20d4c4dad086793adfe0911c6b3047ab4a7f6e29
MohmmaedNajib94 <khorashi94@gmail.com>
2021-12-01 20:35:21 +0100

    div added

--- Taschenrechner.py
+++ Taschenrechner.py
@@ -1,8 +1,13 @@
-#Taschenrechner by Seef 
+#Taschenrechner by Seef und Mohammed
 
 def sum(x,y):return x+y
 def sub(x,y):return x-y
 def mal(x,y):return x*y
+def div(x,y):
+    if y == 0:
+        return None
+    else :
+        return x/y
 
 
 # Testing
@@ -13,3 +18,4 @@ y=12
 print(sum(x,y))
 print(sub(x,y))
 print(mal(x,y))
+print(div(x,y))
