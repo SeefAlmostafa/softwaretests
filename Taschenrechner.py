84c202466d7032dfe2c3286006b9103e985054ce
SeefAlmostafa <83471425+SeefAlmostafa@users.noreply.github.com>
2021-12-01 20:22:02 +0100

    Taschenrechner

---
+++ Taschenrechner.py
@@ -0,0 +1,15 @@
+#Taschenrechner by Seef 
+
+def sum(x,y):return x+y
+def sub(x,y):return x-y
+def mal(x,y):return x*y
+
+
+# Testing
+
+x=12
+y=12
+
+print(sum(x,y))
+print(sub(x,y))
+print(mal(x,y))
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+
+x=10
+y=12
+
+print(sum(x,y))
+print(sub(x,y))
+print(mal(x,y))
